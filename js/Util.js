902a72ab37924b85eb420c911fc52953ed65fd5c
NonlinearFruit <1123benji5813@gmail.com>
2016-01-16 09:09:02 -0500

    onclick note updating

--- js/Util.js
+++ js/Util.js
@@ -10,17 +10,8 @@ Util = {};
 // O N L O A D   S T U F F
 window,onload = function(){
 	Util.gen.createEditor();
-	
-	var libs = Util.fs.getLibraries();
-	Util.txt.setLibraryBrowser(libs);
 
-	var lib = Util.session.getLibrary();
-	var notes = Util.fs.getNotes(lib);
-	Util.txt.setFileBrowser(notes);
-
-	var note = Util.session.getNote();
-	Util.txt.setInputText(Util.fs.getNoteContent(lib, note));
-
+	Util.session.update();
 }
 
 // G E N E R A L   S T U F F
@@ -28,7 +19,9 @@ Util.gen = function(){
 
 	var marked = require('marked');
 
-	// External Functions
+	////////////////////////
+	// External Functions //
+	////////////////////////
 	function render(markdownText){
 		var renderedHTML = marked(markdownText);
 		var preview = document.getElementById('preview');
@@ -79,7 +72,9 @@ Util.fs = function(){
 	var root = __dirname;
 	var notebook = "notebook";
 
-	// Internal Functions
+	////////////////////////
+	// Internal Functions //
+	////////////////////////
 
  	function getDirectories(srcpath) {
 	  return fs.readdirSync(srcpath).filter(function(file) {
@@ -108,7 +103,9 @@ Util.fs = function(){
 		});
 	}
 
-	// External Functions
+	////////////////////////
+	// External Functions //
+	////////////////////////
 
 	function getLibraries() {
 		var notebookDir = path.join(root,notebook);
@@ -125,12 +122,12 @@ Util.fs = function(){
 		return getFileContent(noteDir);
 	}
 
-	function writeNote(library,note,content) {
+	function saveNote(library,note,content) {
 		var noteDir = path.join(root,notebook,library,note);
 		writeFile(noteDir,content);
 	}
 
-	return{getLibraries:getLibraries, getNotes:getNotes, getNoteContent:getNoteContent, writeNote:writeNote}
+	return{getLibraries:getLibraries, getNotes:getNotes, getNoteContent:getNoteContent, saveNote:saveNote}
 }();
 
 // T E X T   S T U F F
@@ -141,9 +138,13 @@ Util.txt = function(){
 	// var regexIgnoreChar = /[ \" \, \. \- \! \? \* \[ \] \d ]/g;
 	var regexIgnoreChar = /[^A-Za-z \s']/g;
 
-	// Internal Functions
+	////////////////////////
+	// Internal Functions //
+	////////////////////////
 
-	// External Functions
+	////////////////////////
+	// External Functions //
+	////////////////////////
 	function clean(txt) {
 		txt = txt.replace(regexIgnoreLine,"");
 		txt = txt.replace(regexIgnoreChar,"");
@@ -162,8 +163,6 @@ Util.txt = function(){
 	}
 
 	function setOutputText(txt) {
-		// var editor = ace.edit('analyze');
-		// editor.setValue(txt);
 		Util.gen.render(txt);
 	}
 
@@ -174,12 +173,14 @@ Util.txt = function(){
 
 	function setFileBrowser(files) {
 		var fileBrowser = document.getElementById("file-browser");
+		fileBrowser.innerHTML = "";
 		for (var i = 0; i < files.length; i++) {
 			var note = files[i];
 			var li = document.createElement("li");
 			var strong = document.createElement("strong");
 			strong.appendChild(document.createTextNode(note));
 			li.appendChild(strong);
+			li.onclick = function(y){ return function(){Util.session.setNote(y);} }(note);
 			li.setAttribute("class","collection-item waves-effect");
 			fileBrowser.appendChild(li);
 		};
@@ -187,11 +188,14 @@ Util.txt = function(){
 
 	function setLibraryBrowser(libs) {
 		var libBrowser = document.getElementById("library-browser");
+		libBrowser.innerHTML = "";
+		// libBrowser.onclick
 		for (var i = 0; i < libs.length; i++) {
 			var lib = libs[i];
 			var li = document.createElement("li");
 			li.appendChild(document.createTextNode(lib));
 			li.setAttribute("class","collection-item");
+			li.onclick = function(y){ return function(){Util.session.setLibrary(y);} }(lib);
 			libBrowser.appendChild(li);
 		};
 	}
@@ -208,7 +212,22 @@ Util.session = function(){
 	var currentLib = "shakespeare";
 	var currentNote = "hamlet.md";
 
-	// Getters
+	////////////////////////
+	// Internal Functions //
+	////////////////////////
+	
+	// Refreshes everything to the current library and note
+	function update() {
+		console.log("Updating\t" + currentLib + "\t"+ currentNote);
+		Util.txt.setLibraryBrowser( Util.fs.getLibraries() );
+		Util.txt.setFileBrowser( Util.fs.getNotes(currentLib) );
+		Util.txt.setInputText( Util.fs.getNoteContent(currentLib,currentNote) );
+		console.log("Updated");
+	}
+
+	/////////////
+	// Getters //
+	/////////////
 	
 	function getLibrary() {
 		return currentLib;
@@ -218,17 +237,38 @@ Util.session = function(){
 		return currentNote;
 	}
 
-	// Setters
+	/////////////
+	// Setters //
+	/////////////
 	
 	function setLibrary(lib) {
-		currentLib = lib;
+		if(lib!=currentLib) {
+			currentLib = lib;
+			var notes = Util.fs.getNotes(currentLib);
+			var note = notes[0];
+			setNote(note);
+		}
 	}
 
 	function setNote(note) {
 		currentNote = note;
+		update();
 	}
 	
-	return{ 
+	return{ update:update,
 		getLibrary:getLibrary, getNote:getNote,
 		setLibrary:setLibrary, setNote:setNote }
 }();
+
+// O N C L I C K   L I S T E N E R S
+Util.onlclick = function(){
+
+	////////////////////////
+	// External Functions //
+	////////////////////////
+	function setup() {
+
+	}
+
+	return{}	
+}
